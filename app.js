4deba6fc8cd86fa2fa6f6d5231bec81584c1ab27
Esto Triramdani N <estolagi@gmail.com>
2022-03-03 18:47:31 +0700

    redeplo

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { partsController, partController } = require('./controllers');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
